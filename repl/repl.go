a974b6d2749ab66f355d9d56e969efdc8cb4ae3e
Henning <mail@henningrck.com>
2023-11-17 12:04:38 +0100

    Extend REPL by adding parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/henningrck/monkey-interpreter/lexer"
-	"github.com/henningrck/monkey-interpreter/token"
+	"github.com/henningrck/monkey-interpreter/parser"
 )
 
 const PROMPT = ">> "
@@ -24,9 +24,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, "Parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
